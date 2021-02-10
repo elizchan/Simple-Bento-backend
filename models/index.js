a18f0343d279548e24bfccfe7ccf60b62b3d7147
elizchan <lizchan@bu.edu>
2021-02-09 22:09:45 -0500

    added db.menu and db.recipe in index.js

--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ const db = {}
 db.mongoose = mongoose
 db.user = require('./user.model')
 db.role = require('./role.model')
+db.menu = require('./menu.model')
+db.recipe = require('./recipes.model')
 
 db.Roles = ['users', 'admin']
 
