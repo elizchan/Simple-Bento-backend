dd3cb452787e86220ab75a3a0240d093bc3c9a32
elizchan <lizchan@bu.edu>
2021-02-10 22:14:00 -0500

    added addrecipe function and psudocode for crud

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -1,1 +1,11 @@
 const db = require('../models/index')
+
+const Recipe = db.recipe;
+const Menu = db.menu;
+
+//display menu from mm/dd/yy-mm/dd/yy
+// exports.displayMenu = (req, res) => {
+//     const
+// }
+
+//edit menu
--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,1 +1,29 @@
 const db = require('../models/index')
+
+const Recipe = db.recipe;
+
+//add recipe to list of available recipes
+exports.addRecipe = (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const dietary = req.body.dietary;
+    const recipe = new Recipe({
+        name,
+        description,
+        recipe
+    });
+    recipe.save((err, recipe)=>{
+        if(err){
+            res.status(500).send({ message: err });
+            return;
+        } else {
+            res.send({
+                message: "recipe was successfully created"
+            })
+        }
+    })
+}
+
+//edit recipes
+
+//delete recipe
